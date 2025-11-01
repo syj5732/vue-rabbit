d492240c0632a459309fdf1fd940470609adf2f6
yujia.sun <chengshi_57@qq.com>
2025-11-01 22:59:18 +0800

    实现Layout吸顶导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,13 +1,16 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
+    <LayoutFixed/>
     <LayoutNav/>
     <LayoutHeader/>
     <router-view/>
+    <div style="height: 1200px;"></div>
     <LayoutFooter/>
 </template>
 <script setup>
 import LayoutFooter from './components/LayoutFooter.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
 import LayoutNav from './components/LayoutNav.vue';
+import LayoutFixed from './components/LayoutFixed.vue';
 </script>
 <style lang="scss" scoped></style>
